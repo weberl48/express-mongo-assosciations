a1d5e46ad917d62a92b8f4d4b02e55c9e340595b
Lucas Weber <weberl48@outlook.com>
2015-08-13 17:05:37 -0600

    basic password validation

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,23 @@ var router = express.Router();
 var db = require('../lib/database.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('new', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 
 router.post("/new-list", function(req,res,next){
-  console.log(req.body);
-
-  db.newList(req.body)
+  db.newList(req.body).then(function(list){
+    console.log(list.list[0]);
+    res.redirect('/dash');
+  });
 });
+
+router.post("/new-user", function(req,res,next){
+    var newUser = db.newUser(req.body);
+    if (newUser[0]){
+      newUser[0].toString();
+      res.render('new',{errors: newUser[0]});
+    }
+});
+
 
 module.exports = router;
